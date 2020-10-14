e06c116b98404b3e2de38a88e25c23243e231636
zinvergo93 <zachary.coding@gmail.com>
2020-10-14 18:00:44 -0500

    added moment

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Zachary Invergo's Portfolio</h1>
+        <h1>Zachary Invergo Portfolio</h1>
 
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
