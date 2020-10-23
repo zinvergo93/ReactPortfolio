7b23c137f86b5ec20438143e80f2640831b397b4
zinvergo93 <zachary.coding@gmail.com>
2020-10-23 13:17:36 -0500

    handle errors on authentication

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -6,7 +6,8 @@ export default class Login extends Component {
         super(props);
         this.state = { 
             email: "",
-            password: ""
+            password: "",
+            errorText: ""
          }
 
         this.handleChange = this.handleChange.bind(this);
@@ -15,7 +16,8 @@ export default class Login extends Component {
 
     handleChange(event){
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
+            errorText: ""
         })
     }
 
@@ -29,8 +31,19 @@ export default class Login extends Component {
         },
         { withCredentials: true }
         ).then(response => {
-            console.log("response", response)
+            if (response.data.status === 'created'){
+                console.log("Welcome!!")
+            } else {
+                this.setState({
+                    errorText: "Wrong email or password"
+                })
+            }
         })
+        .catch(error => {
+           this.setState({
+               errorText: "An error occurred. Please Try again later.s"
+           })
+        });
         event.preventDefault();
     }
 
@@ -38,6 +51,8 @@ export default class Login extends Component {
         return ( 
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+                <div>{this.state.errorText}</div>
                 <form onSubmit={this.handleSubmit}>
                     <input 
                     type = "email"
