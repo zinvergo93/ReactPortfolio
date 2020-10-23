5e1cacaf0f97ee7cd0c13db4cce3ee9cd8e979c3
zinvergo93 <zachary.coding@gmail.com>
2020-10-23 14:56:03 -0500

    render React props

--- src/components/app.js
+++ src/components/app.js
@@ -16,6 +16,29 @@ import NoMatch from './pages/no-match';
 
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this);
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
   render() {
     return (
       <div className='container'>
@@ -26,7 +49,20 @@ export default class App extends Component {
         <NavigationContainer />
          <Switch>
             <Route exact path = "/" component = {Home} />
-            <Route exact path = "/auth" component = {Auth} />
+
+
+            <Route 
+              exact path = "/auth"
+              render={props => (
+                <Auth 
+                  {...props}
+                  handleSuccessfulLogin={this.handleSuccessfulLogin}
+                  handleUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                  />
+                )
+              }
+            />
+
             <Route path = "/about-me" component = {About} />
             <Route path = "/contact" component = {Contact} />
             <Route path = "/blog" component = {Blog} />
